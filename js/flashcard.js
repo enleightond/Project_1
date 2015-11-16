92f54aad54bbc602c953f449cd475ec0f87acb36
enleightond <enleightond@yahoo.com>
2015-11-16 15:23:58 -0800

    First pass on landing page layout

---
+++ js/flashcard.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+var inputName= document.getElementById("name");
+localStorage.setItem("name", inputEmail.value);
+
+var inputEmail= document.getElementById("email");
+localStorage.setItem("email", inputEmail.value);
+
+});
