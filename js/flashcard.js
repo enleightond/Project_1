0124b84e26427d55a0c7af82920a468389f0300b
enleightond <enleightond@yahoo.com>
2015-11-17 12:07:48 -0800

    Added getting started page with dropdown button choices

--- js/flashcard.js
+++ js/flashcard.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 
-var inputName= document.getElementById("name");
-localStorage.setItem("name", inputEmail.value);
+//onClickEvent(){}
 
-var inputEmail= document.getElementById("email");
-localStorage.setItem("email", inputEmail.value);
+$("#startBtn").on('click', function(event){
+	event.preventDefault();
+var userName = $("#name").val(),
+    userEmail = $("#email").val();
+	
+	localStorage.setItem("name",userName);    
+	localStorage.setItem("email",userEmail);
+
+	$('#userInfo').hide();
+	
+});
+
+
+// var inputName= document.getElementById("name");
+// localStorage.setItem("name", inputName.value);
+
+// var inputEmail= document.getElementById("email");
+// localStorage.setItem("email", inputEmail.value);
 
 });
